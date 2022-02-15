e6c1a6a86fe8d169e082db53e97d0131ddd4eb37
Richard Ingwe <richardingwe5@gmail.com>
2022-02-15 03:44:16 +0100

    .

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,14 @@
 //@desc     Get goals
 //@route    GET /api/goals
 //@access   Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
     res.status(200).json({ message: 'Get goals' });
 };
 
 //@desc     Set goals
 //@route    POST /api/goals
 //@access   Private
-const setGoals = (req, res) => {
+const setGoals = async (req, res) => {
     if (!req.body.text) {
         res.status(400);
         throw new Error('Please add a text field');
@@ -20,14 +20,14 @@ const setGoals = (req, res) => {
 //@desc     Update goal
 //@route    PUT /api/goals/:id
 //@access   Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
     res.status(200).json({ message: `Update goals ${req.params.id}` });
 };
 
 //@desc     Delete goal
 //@route    DELETE /api/goals/:id
 //@access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
     res.status(200).json({ message: `Delete goals ${req.params.id}` });
 };
 
