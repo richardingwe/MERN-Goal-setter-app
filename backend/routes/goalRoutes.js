17afe58897424f1849482d288844c2dddd93fc67
Richard Ingwe <richardingwe5@gmail.com>
2022-03-07 20:17:34 +0100

    protected goals routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getGoals, setGoals, updateGoal, deleteGoal } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').get(getGoals).post(setGoals);
+router.route('/').get(protect, getGoals).post(protect, setGoals);
 
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
