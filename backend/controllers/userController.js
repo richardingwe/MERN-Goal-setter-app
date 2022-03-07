25fd4347fe35e8bed7a5be4ca8514e691f34fb9f
Richard Ingwe <richardingwe5@gmail.com>
2022-03-07 20:08:27 +0100

    protect route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get user data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'gotten user data' });
 });
