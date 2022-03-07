36ef586ba50b97277b2e213a8a4c7a6dbca2cc5e
Richard Ingwe <richardingwe5@gmail.com>
2022-03-07 19:49:18 +0100

    implemented login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,6 +52,23 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check for user email
+    const user = await User.findOne({ email });
+
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+
     res.json({ message: 'login user' });
 });
 
