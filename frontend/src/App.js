bb58d81b1e3ab1625bfd37d91bed83eedef33217
Richard Ingwe <richardingwe5@gmail.com>
2022-03-09 21:01:05 +0100

    done with frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
       </Router>
       <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
